a3d98ab437dbc6902ef49b4761c480279535ac53
TRMgit <83818448+TRMgit@users.noreply.github.com>
2022-01-25 12:22:37 +0100

    Først halvbrukbare demo v2

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ print("det Koreanske alfabet. OBS! Du må kunne det Russiske alfabetet fra før 
 vlstrt = input("Vil du starte? y/n ").lower()
 
 while fortsette:
-    if vlstrt == "y":
+    if vlstrt == "y" or "yes" or "ja":
         break
-    elif (vlstrt == "n"): # gjør at alt dette funker
+    elif (vlstrt == "n" or "no" or "nei"): # gjør at alt dette funker
         print("Greit. Start gjerne opp programmet igjen om du vil prøve.")
-        break
+
     else:
-        print("Du tastet inn noe feil. Prøv igjen.") #fisk at den spammer omigjen
+        print("Du tastet inn noe feil. Prøv igjen.") # fisk at den spammer omigjen
 
 print("La oss starte:")
-time.sleep(2)
+time.sleep(3)
 print("Det er 24 bokstaver i det Koreanske alfabetet.")
-time.sleep(2)
+time.sleep(3)
 
 print("Selv om det ser avansert ut er ikke det det mest avanserte å lære.")
 time.sleep(2)
-print("Her er en liste med alle bokstavene og hva de betyr (Selvstuder):")
+print("Her er en liste med alle bokstavene og hva de betyr (Selvstudér):")
 time.sleep(4)
 print("ㄱ    er en G eller K ettersom hva som er før og etter i setningen.")
 time.sleep(0.5)
@@ -36,6 +36,45 @@ time.sleep(0.5)
 print("ㄹ    er ganske vanskelig å lære å lese. Det kan være en L eller R og uttales annerledes basert på bokstav før og etter.")
 time.sleep(0.5)
 print("ㅁ    er en standard M.")
+time.sleep(0.5)
+print("ㅁ    er en standard p eller b avhengig av plassering.")
+time.sleep(0.5)
+print("ㅁ    er en standard s.")
+time.sleep(0.5)
+print("ㅁ    er en standard ng.")
+time.sleep(0.5)
+print("ㅁ    er en standard j.")
+time.sleep(0.5)
+print("ㅁ    er en standard ch.")
+time.sleep(0.5)
+print("ㅁ    er en standard k.")
+time.sleep(0.5)
+print("ㅁ    er en standard t.")
+time.sleep(0.5)
+print("ㅁ    er en standard p.")
+time.sleep(0.5)
+print("ㅁ    er en standard h.")
+time.sleep(0.5)
+print("ㅁ    er en standard a.")
+time.sleep(0.5)
+print("ㅁ    er en  ya og uttales ja.")
+time.sleep(0.5)
+print("ㅁ    er en eo og uttales å.")
+time.sleep(0.5)
+print("ㅁ    er en  yeo og uttales yå.")
+time.sleep(0.5)
+print("ㅁ    er en standard o.")
+time.sleep(0.5)
+print("ㅁ    er en yo og uttales jo.")
+time.sleep(0.5)
+print("ㅁ    er en standard u.")
+time.sleep(0.5)
+print("ㅁ    er en  yu.")
+time.sleep(0.5)
+print("ㅁ    er en eu og uttales u.")
+time.sleep(0.5)
+print("ㅁ    er en standard i.")
+time.sleep(0.5)
 
 input(" ")
 input("Skriv hva som helst for å fortsette når du er klar ")
